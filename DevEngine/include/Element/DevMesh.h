871cca8e5fba75e208270f17d454f0e935e24c1b
DeaDiablo <DeaDiablo@gmail.com>
2011-01-17 17:32:26 +0300

    + Added files class Mesh and Vertex

---
+++ DevEngine/include/Element/DevMesh.h
@@ -0,0 +1,13 @@
+#ifndef DEV_MESH_H
+#define DEV_MESH_H
+
+#include <Kernel/DevInclude.h>
+
+#include <Element/DevVertex.h>
+
+namespace dev
+{
+  
+}
+
+#endif
---
+++ DevEngine/include/Element/DevVertex.h
@@ -0,0 +1,11 @@
+#ifndef DEV_VERTEX_H
+#define DEV_VERTEX_H
+
+#include <Kernel/DevInclude.h>
+
+namespace dev
+{
+  
+}
+
+#endif
