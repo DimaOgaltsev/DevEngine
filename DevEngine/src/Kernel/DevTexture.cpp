7c12931a090d67351aa84ec3f4f7ca1f20328584
DeaDiablo <deadiablo@gmail.com>
2011-09-04 11:54:56 +0400

    * fixed load textures (use d3dx9tex.h)

--- DevEngine/src/Kernel/DevTexture.cpp
+++ DevEngine/src/Kernel/DevTexture.cpp
@@ -2,13 +2,18 @@
 
 #include <Kernel/DevLog.h>
 
+#include <d3dx9tex.h>
+
 using namespace dev;
 
 Texture::Texture(const char* path) :
   _path(path),
   _texture(NULL)
 {
-  if (FAILED(_deviceDX->CreateTexture(128, 128, 0, 0, D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, &_texture, NULL)))
+  //replace load texture
+  //if (FAILED(_deviceDX->CreateTexture(128, 128, 0, 0, D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, &_texture, NULL)))
+  //example: http://code.google.com/p/nvidia-texture-tools/source/browse/tags/2.0.0/src/nvimage/DirectDrawSurface.cpp
+  if (FAILED(D3DXCreateTextureFromFile(_deviceDX, path, &_texture)))
   {
     Log::GetLog()->WriteToLog("Texture not loaded: ");
     Log::GetLog()->WriteToLog(path);
