eab6e9d0ac49453f377bebe7166acc1e401ac983
DeaDiablo <DeaDiablo@gmail.com>
2011-09-07 13:31:36 +0400

    + Added DevShader class

---
+++ DevEngine/include/Shaders/DevShaders.h
@@ -0,0 +1,64 @@
+#ifndef DEV_SHADERS_H
+#define DEV_SHADERS_H
+
+#include <Kernel/DevInclude.h>
+
+namespace dev
+{
+  class Shader
+  {
+  public:
+    enum TypeShader
+    {
+      VS_1_1,
+      VS_2_0,
+      VS_2_a,
+      VS_2_SW,
+      VS_3_0,
+      VS_3_SW,
+
+      PS_1_1,
+      PS_1_2,
+      PS_1_3,
+      PS_1_4,
+      PS_2_0,
+      PS_2_a,
+      PS_2_b,
+      PS_2_SW,
+      PS_3_0,
+      PS_3_SW
+    };
+
+    Shader(const char* path, TypeShader type, const char* fuction = "main");
+    virtual ~Shader();
+
+    virtual void CompileShader();
+
+    inline const char* GetPath() const
+    {
+      return _path.c_str();
+    }
+
+    inline const TypeShader* GetType() const
+    {
+      return &_type;
+    }
+
+    inline const char* GetFunction() const
+    {
+      return _function.c_str();
+    }
+
+  protected:
+
+    const char* getCharType();
+
+    ID3DXConstantTable* _constantTable;
+    ID3DXBuffer*        _shader;
+    std::string         _path;
+    TypeShader          _type;
+    std::string         _function;
+  };
+}
+
+#endif
---
+++ DevEngine/src/Shaders/DevShaders.cpp
@@ -0,0 +1,99 @@
+#include <Shaders/DevShaders.h>
+
+#include <Kernel/DevLog.h>
+
+using namespace dev;
+
+Shader::Shader(const char* path, TypeShader type, const char* fuction) :
+  _shader(NULL),
+  _constantTable(NULL),
+  _path(path),
+  _type(type),
+  _function(fuction)
+{
+  CompileShader();
+}
+
+Shader::~Shader()
+{
+  if (_shader)
+    _shader->Release();
+  if (_constantTable)
+    _constantTable->Release();
+}
+
+void Shader::CompileShader()
+{
+  DWORD flags = D3DXSHADER_OPTIMIZATION_LEVEL3;
+  #ifdef _DEBUG
+    flags = D3DXSHADER_DEBUG;
+  #endif
+
+  ID3DXBuffer* _errorBuffer = NULL;
+  const char* type = getCharType();
+  if (type != NULL && FAILED(D3DXCompileShaderFromFile(_path.c_str(), 0, 0, _function.c_str(), type, flags, &_shader, &_errorBuffer, &_constantTable)))
+  {
+    std::string buffer("Shader (");
+    buffer += _path;
+    buffer += "compilation error: ";
+    if(_errorBuffer)
+    {
+      buffer += (char*)_errorBuffer->GetBufferPointer();
+      Log::GetLog()->WriteToLog(buffer.c_str());
+      _errorBuffer->Release();
+      return;
+    }
+    
+    if (type == NULL)
+    {
+      buffer += "Unknown shader type";
+      Log::GetLog()->WriteToLog(buffer.c_str());
+      return;
+    }
+
+     buffer += "Unknown error";
+     Log::GetLog()->WriteToLog(buffer.c_str());
+     return;
+  }
+}
+
+const char* Shader::getCharType()
+{
+  switch(_type)
+  {
+  case VS_1_1:
+    return "vs_1_1";
+  case VS_2_0:
+    return "vs_2_0";
+  case VS_2_a:
+    return "vs_2_a";
+  case VS_2_SW:
+    return "vs_2_sw";
+  case VS_3_0:
+    return "vs_3_0";
+  case VS_3_SW:
+    return "vs_3_sw";
+
+  case PS_1_1:
+    return "ps_1_1";
+  case PS_1_2:
+    return "ps_1_2";
+  case PS_1_3:
+    return "ps_1_3";
+  case PS_1_4:
+    return "ps_1_4";
+  case PS_2_0:
+    return "ps_2_0";
+  case PS_2_a:
+    return "ps_2_a";
+  case PS_2_b:
+    return "ps_2_b";
+  case PS_2_SW:
+    return "ps_2_sw";
+  case PS_3_0:
+    return "ps_3_0";
+  case PS_3_SW:
+    return "ps_3_sw";
+  }
+  return NULL;
+}
